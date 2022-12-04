98f2f90ddffbf10c54af66e238ba140540e33110
onisim_albarov <onisim93@gmail.com>
2022-12-04 12:03:21 +0400

    complete stage1-module4-collections-task1

--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -5,11 +5,25 @@ import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        sourceList.sort(new ListComparator());
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        int first = Integer.parseInt(a);
+        int second = Integer.parseInt(b);
+        int firstFunction = calculationFunc(first);
+        int secondFunction = calculationFunc(second);
+        if (firstFunction == secondFunction) {
+            return Integer.compare(first, second);
+        }
+        return Integer.compare(firstFunction, secondFunction);
+    }
+
+    private int calculationFunc(int x) {
+        return (int) (5 * Math.pow(x, 2) + 3);
     }
 }
+
